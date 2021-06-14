7bf8572b341eba3ddb98d47deaa3415367432034
Andrey Agrachev <andrey.agrachev@intel.com>
2021-06-14 17:32:44 +0300

    Add basic ocl support

--- aho/ocl_aho.cpp
+++ aho/ocl_aho.cpp
@@ -1,7 +1,20 @@
 #include "ocl_aho.h"
 
-
-int main()
+void set_platform()
 {
-    
+    std::vector<cl::Platform> platforms;
+    cl::Platform::get( &platforms);
+    cl::Platform selected_platform;
+    for ( auto &p : platforms)
+    {
+        std::string platver = p.getInfo<CL_PLATFORM_VERSION>();
+        std::cout << platver << std::endl;
+        if (platver.find("OpenCL 2.") != std::string::npos)
+            selected_platform = p;
+    }
+    assert( selected_platform() != 0 && "No OpenCL 2.0 platform found.\n");
+
+    cl::Platform default_platform = cl::Platform::setDefault( selected_platform);
+
+    assert( default_platform == selected_platform && "Error setting default platform");
 }
--- aho/ocl_aho.h
+++ aho/ocl_aho.h
@@ -1,2 +1,9 @@
 #include "raw_aho.h"
+#include "CL/cl2.hpp"
 
+void set_platform();
+
+int main()
+{
+    set_platform();  
+}
