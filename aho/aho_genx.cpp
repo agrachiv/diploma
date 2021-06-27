e73cc8540fd8e95331de1164dd16ca1ca1d0a452
Andrey Agrachev <andrey.agrachev@intel.com>
2021-06-27 22:30:35 +0300

    Add right version of cm kernel

--- aho/aho_genx.cpp
+++ aho/aho_genx.cpp
@@ -1,38 +1,67 @@
 #include <cm/cm.h>
 
 _GENX_MAIN_ void Find_matches(
-    //__constant char* transition_table,
-    //__global char* matched_patterns,
-    //__constant char* text,
-    //int dictionary_size,
-    //int text_size
+    const SurfaceIndex transition_table [[type("buffer_t char")]],
+    const SurfaceIndex text_buffer [[type("buffer_t char")]],
+    SurfaceIndex matched_patterns_buffer [[type("buffer_t char")]],
+    const int dictionary_size,
+    const int text_size
     )
-{
-    printf("TestTestTestTestTestTestTest");
-    /*
+{   
     unsigned tid = cm_group_id(0) * cm_local_size(0) + cm_local_id(0);
-    */
-}
+    
+    constexpr int MAX_TEXT_SIZE = 1024;
+    constexpr int VECTOR_SIZE = 128;
+    constexpr int EXT_VECTOR_SIZE = 128 + 16; //hack to get small offset
+    int allinged_tid = tid / VECTOR_SIZE;
+    int remainder = tid % VECTOR_SIZE; 
 
-/*
-    int state_index = transition_table[0];
+    //Read text buffer to vector
+    vector<char, EXT_VECTOR_SIZE> ext_text;
+    read( text_buffer, allinged_tid * 128, ext_text.select<128,1>(0));
+    read( text_buffer, allinged_tid * 128 + 128, ext_text.select<16,1>(128));
 
-    for ( int i = id; i < text_size; i++)
+    vector<char, 128> text = ext_text.select<128,1>(remainder);
+
+    //Prepare store buffer vector
+    //vector<char, MAX_TEXT_SIZE> = matched_patterns_info;
+    int next_state = 0;
+    
+    //#pragma unroll
+    for ( int i = 0; i < text_size; i++)
     {
         char symbol = text[i];
-        int next_state = transition_table[state_index + 2 * symbol]; //next_state индекс начала новой строки
-        int matched_pattern = transition_table[state_index + 2 * symbol + 1]; //в решеточку 
-        if ( next_state != 0) //partial match
+        
+        //Get raw (can't read more than 128 at once)
+        vector<char, 256> full_raw;
+        read( transition_table, next_state * 256, full_raw.select<128, 1>(0));  
+        read( transition_table, next_state * 256 + 128, full_raw.select<128, 1>(128));
+
+        //Get states vector
+        vector<char, 128> next_states = full_raw.select<128, 2>(0);
+
+        //Get pattern numbers vector
+        vector<char, 128> matched_patterns = full_raw.select<128, 2>(1);
+
+        //Get next state (raw) number
+        next_state = next_states[symbol];
+        //Get matched pattern number (might be 0)
+        int matched_pattern = matched_patterns[symbol];
+
+        //Partial match
+        if ( next_state != 0)
         {
-            if ( matched_pattern != 0) //full pattern match
-                matched_patterns[ id * dictionary_size + matched_pattern - 1] = '1'; //mark as matched
+            //Full pattern match
+            if ( matched_pattern != 0)
+                //Mark as matched
+                write( matched_patterns_buffer, tid * dictionary_size, matched_pattern - 1, '1');
+            //Longest pattern
             if ( next_state == -1)
                 break;
-            state_index = next_state * 128 * 2;
         }
         else
         {
             break;
         }
     }
-*/
+}
